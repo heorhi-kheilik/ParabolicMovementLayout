c7bb0ca683f83fd88c913f615d3b24b82057a4f9
Heorhi Heilik <heheilik@icloud.com>
2024-08-05 09:42:08 +0300

    RootVC created

---
+++ CardCollection/CardCollection/UI/Root/RootViewController.swift
@@ -0,0 +1,36 @@
+//
+//  RootViewController.swift
+//  CardCollection
+//
+//  Created by Heorhi Heilik on 4.08.24.
+//
+
+import SwiftUI
+import UIKit
+
+final class RootViewController: UIHostingController<RootView> {
+
+    init() {
+        super.init(rootView: RootView())
+        rootView.delegate = self
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
+
+// MARK: - RootViewDelegate
+
+extension RootViewController: @preconcurrency RootViewDelegate {
+
+    func programmaticImplementationTouchUpInside() {
+        print("progammatic")
+    }
+
+    func xibImplementationTouchUpInside() {
+        print("xib")
+    }
+
+}
